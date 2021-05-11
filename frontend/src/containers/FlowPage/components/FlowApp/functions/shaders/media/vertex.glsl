c49f22d30a95138381fd8baf975e116f1ca00d19
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-05-11 09:34:11 +0200

    Update shader

--- frontend/src/containers/FlowPage/components/FlowApp/functions/shaders/media/vertex.glsl
+++ frontend/src/containers/FlowPage/components/FlowApp/functions/shaders/media/vertex.glsl
@@ -10,11 +10,12 @@ uniform vec2 uPlaneSizes;
 varying vec2 vUv;
 
 void main() {
+
+  // vec3 newPos = position;
+  // newPosition.z += sin(newPos.y  * PI + PI / 2.0) * -uStrength;
+
   vec4 newPosition = modelViewMatrix * vec4(position, 1.0);
-
   newPosition.z += sin(newPosition.y / uViewportSizes.y * PI + PI / 2.0) * -uStrength;
-
-  
 
   gl_Position = projectionMatrix * newPosition;
 
