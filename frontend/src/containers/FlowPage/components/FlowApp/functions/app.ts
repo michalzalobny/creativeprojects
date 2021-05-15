223e778b7fb416711f28d9c44815930f58446745
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-05-15 11:28:11 +0200

    Disable antalias

--- frontend/src/containers/FlowPage/components/FlowApp/functions/app.ts
+++ frontend/src/containers/FlowPage/components/FlowApp/functions/app.ts
@@ -110,7 +110,7 @@ export const app = (appProps: App) => {
 
     appObj.renderer = new THREE.WebGLRenderer({
       canvas: appProps.canvasRefEl,
-      antialias: true,
+      // antialias: true,
       alpha: true,
     });
 
