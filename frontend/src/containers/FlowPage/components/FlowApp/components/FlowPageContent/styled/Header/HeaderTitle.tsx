f0770233999055c54716141bd74c40b6dd55f364
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-05-15 18:12:36 +0200

    Add text shadow

--- frontend/src/containers/FlowPage/components/FlowApp/components/FlowPageContent/styled/Header/HeaderTitle.tsx
+++ frontend/src/containers/FlowPage/components/FlowApp/components/FlowPageContent/styled/Header/HeaderTitle.tsx
@@ -9,6 +9,7 @@ export const HeaderTitle = styled(motion.h2)<Props>`
   font-family: 'Playfair';
   color: white;
   font-weight: 400;
+  text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
 
   text-align: center;
   font-size: 12vw;
