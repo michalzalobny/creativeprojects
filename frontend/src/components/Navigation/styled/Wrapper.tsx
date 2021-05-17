1ed2315d97952f0b99b7bf4757c5681621f27d74
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-05-17 14:00:15 +0200

    Update navigation

--- frontend/src/components/Navigation/styled/Wrapper.tsx
+++ frontend/src/components/Navigation/styled/Wrapper.tsx
@@ -5,8 +5,8 @@ interface Props {}
 
 export const Wrapper = styled(motion.div)<Props>`
   position: fixed;
-  bottom: 12px;
-  right: 12px;
+  bottom: 15px;
+  right: 15px;
   background-color: #eeeeee;
   box-shadow: rgba(0, 0, 0, 0.3);
   border-radius: 8px;
