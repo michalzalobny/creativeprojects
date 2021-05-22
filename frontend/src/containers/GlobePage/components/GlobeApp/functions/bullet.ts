4eaf62a625131166616be846343fa055c1ebb780
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-05-22 15:21:51 +0200

    Update pos funciton

--- frontend/src/containers/GlobePage/components/GlobeApp/functions/bullet.ts
+++ frontend/src/containers/GlobePage/components/GlobeApp/functions/bullet.ts
@@ -10,7 +10,7 @@ const calcPosFromLatLonRad = (lat, lon) => {
   //https://en.wikipedia.org/wiki/Spherical_coordinate_system
   const phi = (90 - lat) * (Math.PI / 180);
   const theta = (lon + 180) * (Math.PI / 180);
-  const x = -Math.sin(phi) * Math.cos(theta);
+  const x = -Math.cos(theta) * Math.sin(phi);
   const z = Math.sin(phi) * Math.sin(theta);
   const y = Math.cos(phi);
 
@@ -35,7 +35,7 @@ export const bullet = (): BulletReturn => {
   };
 
   const init = () => {
-    generateBullet(51.7592, 19.456);
+    generateBullet(54.9783, 1.6178);
   };
 
   init();
