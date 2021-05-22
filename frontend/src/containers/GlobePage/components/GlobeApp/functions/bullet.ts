b8b44f9ad3a9ac2c957644b6e84aeb985f1be11c
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-05-22 14:44:13 +0200

    Add map texture

--- frontend/src/containers/GlobePage/components/GlobeApp/functions/bullet.ts
+++ frontend/src/containers/GlobePage/components/GlobeApp/functions/bullet.ts
@@ -10,19 +10,27 @@ export const bullet = (): BulletReturn => {
   const container = new THREE.Object3D();
   container.matrixAutoUpdate = false;
 
-  const geometry = new THREE.SphereBufferGeometry(0.2, 30, 30);
+  const geometry = new THREE.SphereBufferGeometry(0.05, 30, 30);
   let mesh: BulletMesh;
   const globeMaterial = new THREE.MeshBasicMaterial({
     color: new THREE.Color('red'),
   });
 
-  const generateBullet = () => {
+  const generateBullet = (latitude: number, longitude: number) => {
+    //https://en.wikipedia.org/wiki/Spherical_coordinate_system
+    const lat = (latitude * Math.PI) / 180;
+    const lng = (longitude * Math.PI) / 180;
+
+    const x = Math.cos(lng) * Math.sin(lat);
+    const y = Math.sin(lng) * Math.sin(lat);
+    const z = Math.cos(lat);
+
     mesh = new THREE.Mesh(geometry, globeMaterial);
-    mesh.position.set(1 + 0.2, 0, 0);
+    mesh.position.set(x, y, z);
   };
 
   const init = () => {
-    generateBullet();
+    generateBullet(42, 51);
   };
 
   init();
--- frontend/src/containers/GlobePage/components/GlobeApp/functions/globe.ts
+++ frontend/src/containers/GlobePage/components/GlobeApp/functions/globe.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 
+import earthSrc from './images/earth.jpg';
 import { bullet } from './bullet';
 import vertexShader from './shaders/globe/vertex.glsl';
 import fragmentShader from './shaders/globe/fragment.glsl';
@@ -8,15 +9,19 @@ export const globe = () => {
   const container = new THREE.Object3D();
   container.matrixAutoUpdate = false;
 
-  const globeGeoemetry = new THREE.SphereBufferGeometry(1, 30, 30);
+  const geometry = new THREE.SphereBufferGeometry(1, 30, 30);
   let mesh;
-  const globeMaterial = new THREE.ShaderMaterial({
-    vertexShader: vertexShader,
-    fragmentShader: fragmentShader,
+  // const material = new THREE.ShaderMaterial({
+  //   vertexShader: vertexShader,
+  //   fragmentShader: fragmentShader,
+  // });
+
+  const material = new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load(earthSrc.src),
   });
 
   const generateGlobe = () => {
-    mesh = new THREE.Mesh(globeGeoemetry, globeMaterial);
+    mesh = new THREE.Mesh(geometry, material);
     container.add(mesh);
   };
 
