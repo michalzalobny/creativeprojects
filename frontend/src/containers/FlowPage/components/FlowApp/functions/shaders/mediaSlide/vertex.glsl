fa2e4f93d9a380abaaee0f0c4f10093f504c0afa
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-05-25 23:07:25 +0200

    Update side scroll

--- frontend/src/containers/FlowPage/components/FlowApp/functions/shaders/mediaSlide/vertex.glsl
+++ frontend/src/containers/FlowPage/components/FlowApp/functions/shaders/mediaSlide/vertex.glsl
@@ -13,13 +13,13 @@ void main() {
 
 
   vec4 newPosition = modelViewMatrix * vec4(position, 1.0);
-  newPosition.y += sin(newPosition.x / uViewportSizes.x * PI + PI / 2.0) * -uStrength * 120.;
+  newPosition.y += sin(newPosition.x / uViewportSizes.x * PI + PI / 2.0) * -uStrength * 40.;
 
   vec3 newPos = position;
   // newPosition.z += sin(newPos.x  * PI + PI / 2.0) * -uStrength;
 
-  newPosition.x += sin(newPos.y  * PI + PI / 2.0) * -uStrength * 80.;
-  newPosition.y += sin(newPos.x  * PI + PI / 2.0) * abs(-uStrength) * 30.;
+  newPosition.x += sin(newPos.y  * PI + PI / 2.0) * -uStrength * 50.;
+  newPosition.y += sin(newPos.x  * PI + PI / 2.0) * abs(-uStrength) * 20.;
 
   gl_Position = projectionMatrix * newPosition;
 
