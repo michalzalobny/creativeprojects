2791ce24fa4675cf671c7d64e41993355b66f752
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-05-27 23:23:04 +0200

    Update app colors

--- frontend/src/containers/GlobePage/components/GlobeApp/functions/lights.ts
+++ frontend/src/containers/GlobePage/components/GlobeApp/functions/lights.ts
@@ -1,23 +1,31 @@
 import * as THREE from 'three';
 
+const LIGHTS_POWER = 0.7;
+
 export const lights = () => {
   const container = new THREE.Object3D();
   container.matrixAutoUpdate = false;
 
   const generateLights = () => {
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
-    // container.add(ambientLight);
-
-    const directionalLight1 = new THREE.DirectionalLight(0x2e39c6, 3.5);
+    const directionalLight1 = new THREE.DirectionalLight(
+      0x2e39c6,
+      3 * LIGHTS_POWER,
+    );
     directionalLight1.position.set(-3, 0, 3);
     container.add(directionalLight1);
 
-    const directionalLight2 = new THREE.DirectionalLight(0xf30dff, 2.0);
+    const directionalLight2 = new THREE.DirectionalLight(
+      0xf30dff,
+      1.7 * LIGHTS_POWER,
+    );
     directionalLight2.position.set(3, 5, 3);
     container.add(directionalLight2);
 
-    const directionalLight3 = new THREE.DirectionalLight(0x9ea8ff, 2.5);
-    directionalLight3.position.set(-4, 1.2, 3);
+    const directionalLight3 = new THREE.DirectionalLight(
+      0x9ea8ff,
+      1.9 * LIGHTS_POWER,
+    );
+    directionalLight3.position.set(-4, 1.2, 3 * LIGHTS_POWER);
     container.add(directionalLight3);
   };
 
--- frontend/src/containers/GlobePage/components/GlobeApp/styled/Wrapper.tsx
+++ frontend/src/containers/GlobePage/components/GlobeApp/styled/Wrapper.tsx
@@ -9,4 +9,6 @@ export const Wrapper = styled(motion.div)<Props>`
   height: 100%;
   overflow: hidden;
   user-select: none;
+  background: rgb(0, 57, 158);
+  background: linear-gradient(-344deg, #001b4b 0%, #060b1d 50%, #56037c 100%);
 `;
