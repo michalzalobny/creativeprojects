41be4c9df2bfd3b768e7b3571d42ea2437d96c61
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-06-03 15:06:40 +0200

    Update material

--- frontend/src/containers/ParticlesPage/components/App/functions/model.ts
+++ frontend/src/containers/ParticlesPage/components/App/functions/model.ts
@@ -1,5 +1,8 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { MeshSurfaceSampler } from 'three/examples/jsm/math/MeshSurfaceSampler';
+import vertexShader from './shaders/dots/vertex.glsl';
+import fragmentShader from './shaders/dots/fragment.glsl';
 
 interface Model {
   modelSrc: string;
@@ -10,17 +13,59 @@ export const model = ({ loader, modelSrc }: Model) => {
   const container = new THREE.Object3D();
   container.matrixAutoUpdate = false;
 
+  let mesh;
+  let particlesGeometry;
+  const particlesMaterial = new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+  });
+  let particles;
+
   const generateModel = () => {
     loader.load(modelSrc, res => {
-      console.log(res);
+      mesh = res.scene.children[0];
+      updateParticlesGeometry();
+      particles = new THREE.Points(particlesGeometry, particlesMaterial);
+      add();
     });
-    // container.add(directionalLight1);
+  };
+
+  const updateParticlesGeometry = () => {
+    const sampler = new MeshSurfaceSampler(mesh).build();
+    const count = 10000;
+    particlesGeometry = new THREE.BufferGeometry();
+
+    const positionArray = new Float32Array(count * 3);
+    const scaleArray = new Float32Array(count);
+
+    for (let i = 0; i < count; i++) {
+      const newPosition = new THREE.Vector3();
+      sampler.sample(newPosition);
+
+      positionArray[i * 3 + 0] = newPosition.x;
+      positionArray[i * 3 + 1] = newPosition.y;
+      positionArray[i * 3 + 2] = newPosition.z;
+      scaleArray[i] = Math.random();
+    }
+
+    particlesGeometry.setAttribute(
+      'position',
+      new THREE.BufferAttribute(positionArray, 3),
+    );
   };
 
   const init = () => {
     generateModel();
   };
   init();
+
+  const add = () => {
+    container.add(particles);
+  };
+
+  const remove = () => {
+    container.remove(particles);
+  };
 
   return {
     container,
--- frontend/src/containers/ParticlesPage/components/App/functions/shaders/globe/fragment.glsl
+++
@@ -1,6 +0,0 @@
-varying vec2 vUv;
-
-void main(){
-  float strength = 1. - (abs(length(vec2(vUv.x - 0.5,vUv.y - 0.5)) - 0.44) * 9.);
-  gl_FragColor = vec4(0., 0., 0., strength * 0.5);
-}
