9490b6cd2493b57bf8f353f2de199326533a05ca
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-06-06 20:20:38 +0200

    Update index page

---
+++ frontend/src/containers/IndexPage/styled/Text.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+interface Props {}
+
+export const Text = styled(motion.div)<Props>`
+  font-family: 'Playfair';
+  font-size: 6vw;
+`;
