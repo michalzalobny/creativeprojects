42518556e5bc7dd0cbc8792635e8dee534d3432b
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-06-09 09:39:58 +0200

    Update hover effect

--- frontend/src/containers/ParticlesPage/components/App/functions/shaders/mediaSlide/vertex.glsl
+++ frontend/src/containers/ParticlesPage/components/App/functions/shaders/mediaSlide/vertex.glsl
@@ -17,9 +17,9 @@ void main(){
     float dist = distance(position.xy, uMouse3D.xy);
     float area = 1.- smoothstep(0., 80., dist);
 
-    stablePosition.x += 30.*sin(uTime *  aRandom * 0.1) * area * (uSpeed * 0.001 * aRandom + 0.6 );
-    stablePosition.y += 30.*cos(uTime *  aRandom * 0.1) * area ;
-    stablePosition.z += 40.*cos(uTime *  aRandom * 0.1) * area ;
+    stablePosition.x += 30.*sin(uTime *  aRandom * 0.03) * area * (uSpeed * 0.001 * aRandom + 0.6 );
+    stablePosition.y += 30.*cos(uTime *  aRandom * 0.03) * area ;
+    stablePosition.z += 40.*cos(uTime *  aRandom * 0.03) * area ;
 
     vec4 modelPosition = modelMatrix * vec4(stablePosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
