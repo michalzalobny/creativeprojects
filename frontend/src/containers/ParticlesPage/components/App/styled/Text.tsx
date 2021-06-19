68287a7ead08761649dfe8076e5fba8da38f6dd7
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-06-19 07:31:46 +0200

    Update mobile fontsize

--- frontend/src/containers/ParticlesPage/components/App/styled/Text.tsx
+++ frontend/src/containers/ParticlesPage/components/App/styled/Text.tsx
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
+import { media } from 'utils/responsive';
+
 interface Props {}
 
 export const Text = styled(motion.div)<Props>`
-  font-size: 5vw;
+  font-size: 12vw;
   color: white;
   font-family: 'Playfair';
   position: relative;
+
+  ${media.tablet} {
+    font-size: 5vw;
+  }
 `;
