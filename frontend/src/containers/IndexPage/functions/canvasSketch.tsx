76589d241504cc153327bdc83c99965bb7798283
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-06-19 16:17:03 +0200

    Add more elements to 2d canvas

--- frontend/src/containers/IndexPage/functions/canvasSketch.tsx
+++ frontend/src/containers/IndexPage/functions/canvasSketch.tsx
@@ -8,7 +8,28 @@ export interface CanvasSketchReturn {}
 
 export const canvasSketch = ({ appObj }: CanvasSketch): CanvasSketchReturn => {
   const init = () => {
-    appObj.ctx.fillRect(100, 100, 100, 100);
+    const { ctx } = appObj;
+
+    //Rectangle
+    ctx.fillStyle = 'green';
+    ctx.fillRect(100, 100, 100, 100);
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(300, 100, 100, 100);
+
+    //Line
+    ctx.beginPath();
+    ctx.moveTo(50, 300);
+    ctx.lineTo(300, 100);
+    ctx.lineTo(400, 300);
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+
+    //Arc /
+
+    ctx.beginPath();
+    ctx.arc(300, 300, 30, 0, Math.PI * 2, false);
+    ctx.strokeStyle = 'blue';
+    ctx.stroke();
   };
 
   init();
