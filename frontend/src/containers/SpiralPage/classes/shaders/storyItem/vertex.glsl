3879063f58afc44955c709b9535da11a67a563bc
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-08-03 19:56:44 +0200

    Update items shader

--- frontend/src/containers/SpiralPage/classes/shaders/storyItem/vertex.glsl
+++ frontend/src/containers/SpiralPage/classes/shaders/storyItem/vertex.glsl
@@ -10,17 +10,14 @@ uniform vec2 uPlaneSizes;
 varying vec2 vUv;
 
 void main() {
-
-
   vec4 newPosition = modelViewMatrix * vec4(position, 1.0);
   vec3 newPos = position;
 
-  newPosition.x += sin(newPosition.y / uPlaneSizes.y * 0.9 * PI + PI / 2.0) * uStrength * 10.;
-  newPosition.y += sin(newPosition.x / uPlaneSizes.x * 0.9 * PI + PI / 2.0) * uStrength * 25.;
+  newPosition.x += sin(newPosition.y / uPlaneSizes.y * 0.9 * PI + PI / 2.0) * uStrength * 5.;
+  newPosition.y += sin(newPosition.x / uPlaneSizes.x * 0.9 * PI + PI / 2.0) * uStrength * 15.;
 
-  newPosition.y += sin(newPos.x * PI + PI / 2.0) * uStrength * -11.;
-  // newPosition.x += sin(newPos.y * PI + PI / 2.0) * uStrength * 15.;
-  
+  newPosition.x += sin(newPos.y * PI + PI / 2.0) * abs(uStrength)  * newPosition.x;
+  newPosition.y += sin(newPos.x * PI + PI / 2.0) * abs(uStrength)  * newPosition.y;
 
   gl_Position = projectionMatrix * newPosition;
 
