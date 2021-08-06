6c2826ddb446a68a9344ebbe2ff6046b3f090482
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-08-06 11:33:30 +0200

    Update hover parallax strngth

--- frontend/src/containers/SpiralPage/classes/shaders/dots/vertex.glsl
+++ frontend/src/containers/SpiralPage/classes/shaders/dots/vertex.glsl
@@ -18,8 +18,8 @@ void main(){
     vec3 stablePosition = mix(position, position2Copy, 1. - uProgress);
 
      //Parallax mouse animation
-    stablePosition.x -= uMouse3D.x * 0.008;
-    stablePosition.y -= uMouse3D.y * 0.008;
+    stablePosition.x -= uMouse3D.x * 0.009;
+    stablePosition.y -= uMouse3D.y * 0.009;
 
     // Cursor animation
     float dist = distance(position.xy, uMouse3D.xy);
