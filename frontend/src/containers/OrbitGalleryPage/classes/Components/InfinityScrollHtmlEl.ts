bbecbcabceddea122bd91abf3a6472bdabac5311
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-08-26 14:37:17 +0200

    Update initial animation

--- frontend/src/containers/OrbitGalleryPage/classes/Components/InfinityScrollHtmlEl.ts
+++ frontend/src/containers/OrbitGalleryPage/classes/Components/InfinityScrollHtmlEl.ts
@@ -1,4 +1,5 @@
 import { EventDispatcher } from 'three';
+import TWEEN, { Tween } from '@tweenjs/tween.js';
 
 import { ScrollValues, UpdateInfo, Bounds } from '../types';
 
@@ -8,19 +9,25 @@ interface Constructor {
 }
 
 export class InfinityScrollHtmlEl extends EventDispatcher {
+  static isParentAnimated = false;
+
   _elBounds: DOMRect | null = null;
   _parentBounds: DOMRect | null = null;
   _scrollValues: ScrollValues;
   _domEl: HTMLElement;
+  _parentEl: HTMLElement | null = null;
   _extra = 0;
   _rendererBounds: Bounds = { height: 100, width: 100 };
+  _scaleTween: Tween<{ progress: number }> | null = null;
+  _scaleMultiplier = 4;
 
   constructor({ domEl, scrollValues }: Constructor) {
     super();
 
     this._scrollValues = scrollValues; //Passed as a reference obj for better performance
     this._domEl = domEl;
-    this._updateSizes();
+
+    this.animateScale(1);
   }
 
   _updateSizes() {
@@ -29,7 +36,8 @@ export class InfinityScrollHtmlEl extends EventDispatcher {
     this._elBounds = this._domEl.getBoundingClientRect();
 
     if (this._domEl.parentElement) {
-      this._parentBounds = this._domEl.parentElement?.getBoundingClientRect();
+      this._parentBounds = this._domEl.parentElement.getBoundingClientRect();
+      this._parentEl = this._domEl.parentElement;
     }
   }
 
@@ -37,6 +45,33 @@ export class InfinityScrollHtmlEl extends EventDispatcher {
     this._rendererBounds = bounds;
     this._extra = 0;
     this._updateSizes();
+  }
+
+  animateScale(destination: number) {
+    //Animate parent once
+    if (InfinityScrollHtmlEl.isParentAnimated) {
+      return;
+    }
+
+    InfinityScrollHtmlEl.isParentAnimated = true;
+    if (this._scaleTween) {
+      this._scaleTween.stop();
+    }
+
+    this._scaleTween = new TWEEN.Tween({ progress: this._scaleMultiplier })
+      .to({ progress: destination }, 2200)
+      .delay(900)
+      .easing(TWEEN.Easing.Exponential.InOut)
+      .onUpdate(obj => {
+        if (this._parentEl) {
+          this._parentEl.style.transform = `translate(-50%, -50%) rotate(-90deg) scale(${obj.progress})`;
+        }
+      })
+      .onComplete(() => {
+        this._updateSizes();
+      });
+
+    this._scaleTween.start();
   }
 
   update(updateInfo: UpdateInfo) {
--- frontend/src/containers/OrbitGalleryPage/styled/Gallery/TextsContainer.tsx
+++ frontend/src/containers/OrbitGalleryPage/styled/Gallery/TextsContainer.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
+import { tween } from 'components/Animations/framerTransitions';
+
 interface Props {}
 
 export const TextsContainer = styled(motion.div)<Props>`
@@ -9,5 +11,24 @@ export const TextsContainer = styled(motion.div)<Props>`
   z-index: 2;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%) rotate(-90deg) translateX(0px);
+  transform: translate(-50%, -50%) rotate(-90deg);
 `;
+
+TextsContainer.defaultProps = {
+  variants: {
+    initial: {
+      opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+    },
+  },
+  initial: 'initial',
+  animate: 'animate',
+
+  transition: {
+    delay: 0.8,
+    duration: 1,
+    type: 'tween',
+  },
+};
