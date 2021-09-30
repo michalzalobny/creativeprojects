5588ce4f0a23888a3e343f3aab5cebf7f266a958
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-09-30 17:35:07 +0200

    Update 404

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,16 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { useRouter } from 'next/router';
 
 export default function Error404() {
   const router = useRouter();
 
-  const goToLanding = () => {
+  const goToLanding = useCallback(() => {
     router.push('/');
-  };
+  }, [router]);
 
   useEffect(() => {
     goToLanding();
-  }, []);
+  }, [goToLanding]);
 
   return null;
 }
