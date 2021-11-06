333a1d5893b191bf4b4fe9fd8915609d84ca8946
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-11-06 13:01:57 +0100

    Update meta

--- frontend/src/containers/PlaygroundGalleryPage/classes/Singletons/TouchPinch.ts
+++ frontend/src/containers/PlaygroundGalleryPage/classes/Singletons/TouchPinch.ts
@@ -80,6 +80,12 @@ export class TouchPinch extends EventDispatcher {
   };
 
   _addEvents() {
+    const meta = document.createElement('meta');
+    meta.name = 'viewport';
+    meta.content =
+      'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
+    document.getElementsByTagName('head')[0].appendChild(meta);
+
     window.addEventListener('touchstart', this._onTouchDown);
     window.addEventListener('touchmove', this._onTouchMove, {
       passive: false,
