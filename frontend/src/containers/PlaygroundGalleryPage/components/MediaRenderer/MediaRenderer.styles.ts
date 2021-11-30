55081781cd84a2bff70d8234d5672bad141e3ca0
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-11-30 14:49:48 +0000

    Add media preloader

--- frontend/src/containers/PlaygroundGalleryPage/components/MediaRenderer/MediaRenderer.styles.ts
+++ frontend/src/containers/PlaygroundGalleryPage/components/MediaRenderer/MediaRenderer.styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled(motion.div)``;
 
 export const Video = styled.video`
   position: absolute;
--- frontend/src/containers/PlaygroundGalleryPage/components/MediaRenderer/MediaRenderer.tsx
+++ frontend/src/containers/PlaygroundGalleryPage/components/MediaRenderer/MediaRenderer.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+
+import { useMediaPreload } from '../../hooks/useMediaPreload';
 
 import * as S from './MediaRenderer.styles';
 
@@ -10,9 +12,30 @@ export interface Props {
 export const MediaRenderer = (props: Props) => {
   const { mediaSrc, mediaType, ...rest } = props;
 
+  const isImage = useMemo(() => mediaType === 'image', [mediaType]);
+
+  const { isLoaded } = useMediaPreload({
+    mediaSrc,
+    isImage,
+  });
+
+  const wrapperV = {
+    initial: {
+      opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+    },
+  };
+
   return (
     <>
-      <S.Wrapper {...rest}>
+      <S.Wrapper
+        initial="initial"
+        animate={isLoaded ? 'animate' : 'initial'}
+        variants={wrapperV}
+        {...rest}
+      >
         {mediaType === 'video' ? (
           <S.Video autoPlay playsInline loop muted>
             <source src={mediaSrc} type="video/mp4" />
