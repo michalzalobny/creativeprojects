c75c3d0ba976c63b15c3706c529c1773c6df10ac
Michal Zalobny <69809353+michalzalobny@users.noreply.github.com>
2022-07-26 11:31:11 +0200

    Optimize framer motion

--- frontend/src/containers/ConstellationPage/styled/CanvasWrapper.tsx
+++ frontend/src/containers/ConstellationPage/styled/CanvasWrapper.tsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
-interface Props {}
+interface Props { }
 
-export const CanvasWrapper = styled(motion.div)<Props>`
+export const CanvasWrapper = styled.div<Props>`
   width: 100%;
   height: 100%;
   position: fixed;
