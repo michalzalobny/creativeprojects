b6dd4df13d51ae96339a8bc0f26dd02d57917cb8
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2022-08-14 23:40:20 +0200

    Remove motion component from Wrapper

--- frontend/src/containers/ParticlesPage/styled/Wrapper.tsx
+++ frontend/src/containers/ParticlesPage/styled/Wrapper.tsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
 
 interface Props {}
 
-export const Wrapper = styled(motion.div)<Props>`
+export const Wrapper = styled.div<Props>`
   position: relative;
   width: 100%;
   height: 100%;
